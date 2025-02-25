7570a254ddac5b3d1325ae1eb6f529054a4cd2f7
Kevin <kevin5094@hotmail.com>
2025-02-24 21:13:44 -0600

    Week_17_project

--- Module_1/Week_17/Personal_Finance/category.py
+++ Module_1/Week_17/Personal_Finance/category.py
@@ -27,13 +27,3 @@ def manage_categories():
                 window["-CATEGORY_LIST-"].update(values=categories)
 
     window.close()
-
-# def add_category():
-#     loaded_data = data.load_data()
-#     new_category = sg.popup_get_text("Enter category name:")
-
-
-#     if new_category and new_category not in loaded_data["categories"]:
-#         loaded_data["categories"].append(new_category)
-#         data.save_data(loaded_data)
-#         sg.popup("Category saved!")
--- Module_1/Week_17/Personal_Finance/transaction.py
+++ Module_1/Week_17/Personal_Finance/transaction.py
@@ -30,20 +30,3 @@ def add_transaction(transaction_type):
 
 
 
-#     if not finance_data["categories"]:
-#         sg.popup_error("Please add a category before continuing.")
-#         return
-
-#     title = sg.popup_get_text("Enter transaction title:")
-#     amount = sg.popup_get_text("Enter transaction amount:")
-#     category = sg.popup_get_text("Enter transaction category:")
-
-#     if title and amount.isdigit() and category:
-#         finance_data["transactions"].append({
-#             "type": transaction_type,
-#             "title": title,
-#             "amount": int(amount),
-#             "category": category
-#         })
-#         data.save_data(finance_data)
-#         sg.popup("Transaction saved!") 
